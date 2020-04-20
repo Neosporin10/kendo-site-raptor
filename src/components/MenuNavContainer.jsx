3b806e988dbe6b4a52db6b16bfc41b5f781e0ecb
Neosporin10 <mcritzer10@gmail.com>
2020-04-19 20:00:27 -0400

    1 file modified

--- src/components/MenuNavContainer.jsx
+++ src/components/MenuNavContainer.jsx
@@ -7,8 +7,8 @@ const MenuNavContainer = (props) => {
     return (
         <Menu>
             <MenuItem text="Home" render={()=> <Link to={'/'}>Home</Link>}/>
-            <MenuItem text="Grid" render={()=> <Link to={'/grid'}>Grid</Link>}/>
-            <MenuItem text="Chart" render={()=> <Link to={'/chart'}>Chart</Link>}/>
+            <MenuItem text="Blog" render={()=> <Link to={'/blog'}>Blog</Link>}/>
+            <MenuItem text="Chart" render={()=> <Link to={'/concrete'}>Concrete Calculator</Link>}/>
             <MenuItem text="Forms" render={()=> <Link to={'/forms'}>Forms</Link>}/>
         </Menu>
     );
